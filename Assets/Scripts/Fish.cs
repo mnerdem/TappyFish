17591b9a426bfae3d2781a0bd37a3d1739a89569
Merve Nur Erdem <mervenurerdem@hotmail.com>
2023-05-24 20:57:35 +0300

    Fish movements
    Fish için y ekseni üzerinde hareket ve hareket anında angle oluşturuldu.
    Ekran üst kenar kısıtlayıcı koyuldu.

---
+++ Assets/Scripts/Fish.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody2D _rb;
+    public float speed;
+    int angle;
+    int maxAngle = 20;
+    int minAngle = -60;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _rb.velocity = new Vector2(_rb.velocity.x, speed);
+
+        }
+
+        if (_rb.velocity.y > 0)
+        {
+
+            if (angle <= maxAngle)
+            {
+                angle = angle + 4;
+            }
+
+        }
+        else if (_rb.velocity.y < -2.5f)
+        {
+            if (angle > minAngle)
+            {
+                angle = angle - 2;
+            }
+            
+        }
+
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+}
